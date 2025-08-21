1003247fe1313a936c0476c8136e0da02fcad79b
zhoujingren <zhoujingren@bytedance.com>
2025-08-21 20:31:07 +0800

    feat: test commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,19 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', // 新功能
+      'fix', // 修复bug
+      'docs', // 文档更新
+      'style', // 代码格式修改
+      'refactor', // 代码重构
+      'perf', // 性能优化
+      'test', // 测试相关
+      'build', // 构建相关
+      'ci', // CI/CD相关
+      'chore', // 其他修改
+      'revert', // 回滚
+    ],
+    ],
+  },
+};
